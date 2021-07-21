8d2c3686ba7250310b3b1cc1cfd950382cac2ee6
Yomar Miranda <mirandayomar@gmail.com>
2021-07-20 23:22:28 -0500

    feat: create AuthGuard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
+  // { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: '', component: DashboardComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
