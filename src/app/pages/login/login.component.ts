24d5208567e06af8b61b7c83c052ea44066a222f
Yomar Miranda <mirandayomar@gmail.com>
2021-07-20 23:01:45 -0500

    feat: redirect user to dashboard after login.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(4)]],
@@ -18,6 +24,10 @@ export class LoginComponent {
   login() {
     const { email, password } = this.loginForm.value;
     console.log(email, password);
+    this.authService.login(email, password).subscribe((resp) => {
+      this.authService.token = resp;
+      this.router.navigate(['/']);
+    });
     this.loginForm.reset();
   }
 }
