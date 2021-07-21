5e93c9cb0c88b1c38fe320221a1d75a6af462f5c
Yomar Miranda <mirandayomar@gmail.com>
2021-07-20 22:32:42 -0500

    feat: create dashboard view.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
+  linksForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.linksForm = this.formBuilder.group({
+      url: ['', Validators.required],
+      name: ['', Validators.required],
+    });
   }
 
+  registerLink() {
+    const { url, name } = this.linksForm.value;
+    console.log(url, name);
+  }
 }
